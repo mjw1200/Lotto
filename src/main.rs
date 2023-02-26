42ed3be6fede83fec4736e8733f360286da23c51
Mike Waldron <91098610+mjw-optimizely@users.noreply.github.com>
2023-02-26 15:17:58 -0700

    Remove reliance on PICKS=5

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use rand::{thread_rng, Rng, rngs::ThreadRng};
 
-const PICKS: usize = 5; // Numbers in one draw. MT Cash = 5
-const DRAWS: usize = 100; // Number of draws
+const PICKS: usize = 5; // Numbers in one draw. Montana Cash = 5
+const DRAWS: usize = 100; // Number of times you're playing
 
 fn main() {
     let mut rng = thread_rng();
@@ -27,10 +27,7 @@ fn generate(rng: &mut ThreadRng) -> Vec<Vec<u8>> {
 }
 
 fn check(rng: &mut ThreadRng, draws: Vec<Vec<u8>>) {
-    let mut match_two: u32 = 0;
-    let mut match_three: u32 = 0;
-    let mut match_four: u32 = 0;
-    let mut match_five: u32 = 0;
+    let mut matches: [u32; PICKS] = [0; PICKS];
     
     // println!("Draws: {:?}", draws);
 
@@ -43,31 +40,23 @@ fn check(rng: &mut ThreadRng, draws: Vec<Vec<u8>>) {
     // println!("Winners: {:?}", winners);
 
     for i in 0..DRAWS {
-        let mut matches: u8 = 0;
+        let mut match_count: usize = 0;
 
         for j in 0..PICKS {
             if winners[j] == draws[i][j] {
-                matches += 1;
+                match_count += 1;
             }
         }
 
-        if matches == 2 {
-            match_two += 1;
-        }
-        else if matches == 3 {
-            match_three += 1;
-        }
-        else if matches == 4 {
-            match_four += 1;
-        }
-        else if matches == 5 {
-            match_five += 1;
-        }        
+        matches[match_count] += 1;
     }
 
-    println!("Out of {} draws:", DRAWS);
-    println!("Matched two: {}", match_two);
-    println!("Matched three: {}", match_three);
-    println!("Matched four: {}", match_four);
-    println!("Matched five: {}", match_five);
+    println!("Out of {} draws:\n", DRAWS);
+    let mut match_sum: u32 = 0;
+    for i in 0..PICKS {
+        println!("Matched {}: {}", i, matches[i]);
+        match_sum += matches[i];
+    }
+
+    assert_eq!(DRAWS as u32, match_sum);
 }
