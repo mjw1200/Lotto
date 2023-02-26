f3ca91817e3e06636570f5d56418514d1aa8b4eb
Mike Waldron <91098610+mjw-optimizely@users.noreply.github.com>
2023-02-25 21:17:37 -0700

    Will generate and check in one operation; in progress

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,46 @@
-use std::io::{self, Write};
+use std::io::{Write};
 use rand::{thread_rng, Rng};
 use std::fs::File;
+use array2d::{Array2D};
 
 fn main() {
-    print!("Generate (g) or check (c)? ");
-    io::stdout().flush().unwrap();
-    
-    let mut buffer = String::new();
-    let stdin = io::stdin(); // We get `Stdin` here.
-    stdin.read_line(&mut buffer).unwrap();
+    const PICKS: usize = 5; // Numbers in one draw. MT Cash = 5
+    const DRAWS: usize = 5; // Number of draws
 
-    if buffer.starts_with("g") {
-        generate()
-    }
-    else if buffer.starts_with("c") {
-        check()
-    }
-}
-
-fn generate() {
     let mut rng = thread_rng();
-    let mut picks: [u8; 5] = [0; 5];
     let mut outfile = File::create("foo.txt").unwrap();
+    
+    let mut prefilled:Array2D<u8> = Array2D::filled_with(0, DRAWS, PICKS);
 
-    for _ in 0..10 {
-        for j in 0..5 {
-            picks[j] = rng.gen_range(1..45);  
+    for i in 0..DRAWS {
+        for j in 0..PICKS {
+            prefilled.set(i, j, rng.gen_range(1..45)).unwrap();
         }
-        picks.sort(); 
-        output(&mut outfile, picks);     
-        outfile.write(b"\n").unwrap();
+
+        // let mut array: [u8; 5] = prefilled[i];
+
+        // let mut vecvec = prefilled.as_rows();
+        // vecvec[i].sort();
+        // println!("{:?}", vecvec[i])
     }
 
-    outfile.flush().unwrap();
+    let mut rows = prefilled.as_rows();
+    for i in 0..rows.len() {
+        let _ = &rows[i].sort();
+    }
+
+    println!("{:?}", rows);
+
+    // for _ in 0..10 {
+    //     for j in 0..5 {
+    //         picks[j] = rng.gen_range(1..45);  
+    //     }
+    //     picks.sort(); 
+    //     output(&mut outfile, picks);     
+    //     outfile.write(b"\n").unwrap();
+    // }
+
+    // outfile.flush().unwrap();
 }
 
 fn output(of: &mut File, picks : [u8; 5]) {
@@ -41,8 +49,4 @@ fn output(of: &mut File, picks : [u8; 5]) {
         write!(of, "{} ", picks[i]).unwrap();
     }
     println!();
-}
-
-fn check() {
-    println!("Check")
 }
