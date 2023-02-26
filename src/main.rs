52a94faec0852f408006be7c104ccc234eeb10d5
Mike Waldron <91098610+mjw-optimizely@users.noreply.github.com>
2023-02-26 10:39:22 -0700

    Can generate and check. Ha!

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
-use rand::{thread_rng, Rng};
+use rand::{thread_rng, Rng, rngs::ThreadRng};
 
 const PICKS: usize = 5; // Numbers in one draw. MT Cash = 5
-const DRAWS: usize = 5; // Number of draws
+const DRAWS: usize = 3; // Number of draws
 
 fn main() {
-    let vec = generate();
-    println!("{:?}", vec);
+    let mut rng = thread_rng();
+    let draws = generate(&mut rng);
+    check(&mut rng, draws);
 }
 
-fn generate() -> Vec<Vec<u8>> {
-    let mut rng = thread_rng();
-
-    let mut vec: Vec<Vec<u8>> = Vec::new();
+fn generate(rng: &mut ThreadRng) -> Vec<Vec<u8>> {
+    let mut draws: Vec<Vec<u8>> = Vec::new();
     
     for _i in 0..DRAWS {
         let mut draw = Vec::new();
@@ -21,8 +20,33 @@ fn generate() -> Vec<Vec<u8>> {
         }
 
         draw.sort();
-        vec.push(draw);
+        draws.push(draw);
     } 
 
-    return vec;
+    draws
+}
+
+fn check(rng: &mut ThreadRng, draws: Vec<Vec<u8>>) {
+    println!("Draws: {:?}", draws);
+
+    let mut winners: [u8; PICKS] = [0; PICKS];
+
+    for i in 0..PICKS {
+        winners[i] = rng.gen_range(1..45);
+    }
+    winners.sort();
+    println!("Winners: {:?}", winners);
+
+    for i in 0..DRAWS {
+        let mut matches: u8 = 0;
+
+        for 
+        j in 0..PICKS {
+            if winners[j] == draws[i][j] {
+                matches += 1;
+            }
+        }
+
+        println!("{} matches", matches);
+    } 
 }
