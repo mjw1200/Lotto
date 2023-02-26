a9777f70ec14fbb5cc082cfc6dc6fe4ec0b737e0
Mike Waldron <91098610+mjw-optimizely@users.noreply.github.com>
2023-02-26 10:47:33 -0700

    There, that might be it

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use rand::{thread_rng, Rng, rngs::ThreadRng};
 
 const PICKS: usize = 5; // Numbers in one draw. MT Cash = 5
-const DRAWS: usize = 3; // Number of draws
+const DRAWS: usize = 1000000; // Number of draws
 
 fn main() {
     let mut rng = thread_rng();
@@ -27,7 +27,12 @@ fn generate(rng: &mut ThreadRng) -> Vec<Vec<u8>> {
 }
 
 fn check(rng: &mut ThreadRng, draws: Vec<Vec<u8>>) {
-    println!("Draws: {:?}", draws);
+    let mut match_two: u32 = 0;
+    let mut match_three: u32 = 0;
+    let mut match_four: u32 = 0;
+    let mut match_five: u32 = 0;
+    
+    // println!("Draws: {:?}", draws);
 
     let mut winners: [u8; PICKS] = [0; PICKS];
 
@@ -35,18 +40,33 @@ fn check(rng: &mut ThreadRng, draws: Vec<Vec<u8>>) {
         winners[i] = rng.gen_range(1..45);
     }
     winners.sort();
-    println!("Winners: {:?}", winners);
+    // println!("Winners: {:?}", winners);
 
     for i in 0..DRAWS {
         let mut matches: u8 = 0;
 
-        for 
-        j in 0..PICKS {
+        for j in 0..PICKS {
             if winners[j] == draws[i][j] {
                 matches += 1;
             }
         }
 
-        println!("{} matches", matches);
-    } 
+        if matches == 2 {
+            match_two += 1;
+        }
+        else if matches == 3 {
+            match_three += 1;
+        }
+        else if matches == 4 {
+            match_four += 1;
+        }
+        else if matches == 5 {
+            match_five += 1;
+        }        
+    }
+
+    println!("Matched two: {}", match_two);
+    println!("Matched three: {}", match_three);
+    println!("Matched four: {}", match_four);
+    println!("Matched five: {}", match_five);
 }
