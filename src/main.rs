79c7fe53eb71134e94e836a4945e703af9e3a8ae
Mike Waldron <91098610+mjw-optimizely@users.noreply.github.com>
2023-03-01 16:47:01 -0700

    Got the audit trail working, figured out result handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use rand::{thread_rng, Rng, rngs::ThreadRng};
+use std::fs::File;
+use std::io::prelude::*;
 
 const PICKS: usize = 5; // Numbers in one draw. Montana Cash = 5
 const DRAWS: usize = 100; // Number of times you're playing
@@ -59,4 +61,35 @@ fn check(rng: &mut ThreadRng, draws: Vec<Vec<u8>>) {
     }
 
     assert_eq!(DRAWS as u32, match_sum);
+
+    match file_output(winners, draws) {
+        Err(e) => println!("{:?}", e),
+        _ => ()
+    }
+}
+
+fn file_output(winners: [u8; PICKS], draws: Vec<Vec<u8>>) -> std::io::Result<()> {
+    let mut file = File::create("audit_trail.txt")?;
+    let mut output = String::new();
+
+    output += "Winners: ";
+    for winner in winners {
+        let mut winner_string = winner.to_string();
+        winner_string += " ";
+        output += &winner_string;
+    }
+    output += "\n\nDraws:\n";
+    for draw in draws {
+        for dr in draw {
+            let mut draw_string = dr.to_string();
+            draw_string += " ";
+            output += &draw_string
+        }
+        output += "\n";
+    }
+    output += "\n";
+
+    file.write_all(output.as_bytes())?;
+
+    Ok(())
 }
